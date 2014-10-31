366e2794a5024e34b0c53246c9185f2d1b0f867c
Code Division <codedivision@11th.ventures>
2014-10-31 16:23:02 +0800

    log out and create new account works

---
+++ app/controllers/tweets.rb
@@ -0,0 +1,20 @@
+#Index - Show All
+get '/tweets' do
+  @tweets = Tweet.all
+  erb :'tweets/index'
+end
+
+
+#Create
+post '/tweets' do
+  user = User.find_by(id: session[:user_id])
+  new_tweet = Tweet.create(tweet: params[:post], user_id: user.id)
+
+  redirect '/users'
+end
+
+#Delete
+# delete '/tweets/:id' do
+#   Tweet.find(params[:id]).destroy
+#   redirect 'tweets/show'
+# end
---
+++ app/controllers/users.rb
@@ -0,0 +1,35 @@
+# Show - Index
+get '/users' do
+  tweets_array = []
+  @user = User.find_by(id: session[:user_id])
+  @tweets_array = @user.tweets
+  erb :'/users/show'
+end
+
+
+#Create
+post '/users' do
+  if params[:submit] == "create"
+    User.create(username: params[:username], password: params[:password])
+    redirect "/?notice=success"
+  else
+    # byebug
+    if User.valid?(params[:username])
+      new_user = User.find_by(username: params[:username])
+
+      if new_user.authenticate(params[:password])
+        session[:user_id] = new_user.id
+        redirect "/tweets"
+      else
+        redirect "/?notice=wrong_password"
+      end
+    else
+      redirect "/?notice=no_account"
+    end
+  end
+end
+
+delete '/users/logout' do
+  session.clear
+  redirect '/?logout_success'
+end
