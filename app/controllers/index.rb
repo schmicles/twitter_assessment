ca8b360569e16d4551d421e2460ddde488896f81
Code Division <codedivision@11th.ventures>
2014-10-31 16:17:02 +0800

    submit tweet works, view all tweets works, view all tweets from user works

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,31 +1,9 @@
 get '/' do
   # Look in app/views/index.erb
-  if session[:username] == nil
-    case params[:notice]
-    when "success"
-      @color = "green"
-      @message = "Account succesfully created, please login"
-    when "wrong_password"
-      @color = "red"
-      @message = "The password you entered is incorrect"
-    when "no_account"
-      @color = "red"
-      @message = "The account you entered is invalid"
-    when "no_login"
-      @message = "Please login to view content"
-      @color = "red"
-    when "logout_success"
-      @message = "You have been succesfully logged out"
-      @color = "green"
-    else
-      @message = ""
-    end
-
+  if session[:user_id] == nil
     erb :index
-
   else
-
-  redirect :tweets
+    redirect '/tweets'
   end
 end
 
---
+++ db/seeds.rb
@@ -0,0 +1,14 @@
+# def create_users
+#   User.create(username: "dill", password: "abc")
+#   User.create(username: "jambu", password: "abc")
+# end
+
+# create_users
+
+# def create_tweets
+#   Tweet.create(user_id: 1, tweet: "test again")
+#   Tweet.create(user_id: 2, tweet: "banana")
+#   Tweet.create(user_id: 3, tweet: "busy")
+# end
+
+# create_tweets
